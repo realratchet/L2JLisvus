4b7f45f8efb95046723ebddea27c4e5cce3c8570
DnR <4870809-TheDnR@users.noreply.gitlab.com>
2023-05-01 01:13:55 +0300

    chore: Cleanup for RestartResponse packet

--- core/java/net/sf/l2j/gameserver/network/serverpackets/RestartResponse.java
+++ core/java/net/sf/l2j/gameserver/network/serverpackets/RestartResponse.java
@@ -21,20 +21,12 @@ package net.sf.l2j.gameserver.network.serverpackets;
 public class RestartResponse extends L2GameServerPacket
 {
 	private static final String _S__74_RESTARTRESPONSE = "[S] 74 RestartResponse";
-	private final String _message;
-	
-	public RestartResponse()
-	{
-		_message = "ok merong~ khaha";
-	}
 	
 	@Override
 	protected final void writeImpl()
 	{
 		writeC(0x5f);
-		
 		writeD(0x01); // 01-ok
-		writeS(_message);
 	}
 	
 	/*
