83c2aabbc165959c618af22ec97a96c1fc3c31ad
DnR <4870809-TheDnR@users.noreply.gitlab.com>
2023-12-30 23:03:57 +0200

    feat: Functional method getName() for items

--- core/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
+++ core/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
@@ -158,6 +158,8 @@ public final class L2ItemInstance extends L2Object
 		_item = ItemTable.getInstance().getTemplate(itemId);
 		if (_itemId == 0 || _item == null)
 			throw new IllegalArgumentException();
+
+		super.setName(_item.getName());
 		_count = 1;
 		_loc = ItemLocation.VOID;
 		_type1 = 0;
